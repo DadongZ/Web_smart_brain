15dba71e9818ed4f0394b73a9aabee3e5267a7cb
DadongZ <ddzhangzz@gmail.com>
2020-07-26 13:58:28 -0400

    Tilt

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Navigation />
+    </div>
+  );
+}
+
+export default App;
